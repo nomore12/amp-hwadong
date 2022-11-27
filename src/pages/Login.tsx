296d9ce97d91c47573fefbe4118da1eb82f18a63
Sungho Noh <nightwing@naver.com>
2022-11-27 21:20:25 +0900

    post, fetch 모두 성공!!!!!!!!!!!!!!

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,55 @@
-// @flow
-import * as React from 'react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import React, { useEffect, useState } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { API, Amplify, graphqlOperation } from 'aws-amplify';
+import { createPosts, updatePosts, deletePosts } from '../graphql/mutations';
+import { listPosts } from '../graphql/queries';
+import config from '../aws-exports';
+Amplify.configure(config);
 
-function Login() {
+const Login = () => {
+  const [list, setList] = useState([]);
+
+  const fetchPost = async () => {
+    const posts = await API.graphql(graphqlOperation(listPosts));
+    // setList(typeof posts);
+    const { data } = { ...posts } as any;
+    console.log(typeof data.listPosts.items[0].title);
+    setList(data.listPosts.items);
+  };
+
+  useEffect(() => {
+    fetchPost()
+      .then((data) => console.log('data: ', data))
+      .catch((err) => console.log('err', err));
+  }, []);
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Authenticator loginMechanisms={['email']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <div>
+            {list &&
+              list
+                .sort((prev, next) =>
+                  prev['createdAt'] < next['createdAt'] ? -1 : 1
+                )
+                .map((item, index) => {
+                  return (
+                    <div key={index} style={{ border: '1px solid black' }}>
+                      <div>{item['createdAt']}</div>
+                      <div>{item['title']}</div>
+                      <div>{item['desc']}</div>
+                    </div>
+                  );
+                })}
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
-}
+};
 
-export default withAuthenticator(Login);
+export default Login;
