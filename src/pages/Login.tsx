c11edf79dbba2ef2feb42bf68b4cb7e77059d946
Sungho Noh <nightwing@naver.com>
2022-11-27 18:20:52 +0900

    add package

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,11 @@
 // @flow
 import * as React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <AmplifySignOut />
     </div>
   );
 }
